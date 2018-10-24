f5adad836b32524c73ab8c856b604cb16e98af7e
Vit Krajicek <vit@krajicek.org>
2018-10-24 08:38:28 +0200

    heroku db production setup

--- server/config/config.js
+++ server/config/config.js
@@ -1,32 +1,25 @@
-//aby nebyly citlive udaje z configu jako cisla portu, adresy databazi apod pristupne venku (napr na githubu), tak se ty udaje presunou do jsonu, ktery se
-//tady importuje a pokud je env test nebo development tak se pouziji ty udaje z lokalniho config.json
-
-//env test je nastavene v package.json kdyz se spousti testovani
-
-
-//pokud je env production (na heroku) tak neni if splneno a vubec to neprobehne a heroku si to nastavuje samo
-
-
+//this file is omitted from git for security
+// env variable is for setting environment variables which are different for development, test and production
+// env or process.env.NODE_ENV is set to production here by default or is set to test from package.json when testing or is set to production by heroku
+// if process.env.NODE_ENV is production then this code will not run and other environment variables will be set by heroku
 
 let env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
+  
+  // when requiring JSON file, it is automatically parsed to object, bracket notation config[env] is equal to config.development or config.test depending on value of env variable (set by heroku or package.json)
+  //envConfig is object object with local environment values for particular environment (based on env variable) => envConfig is test or development object according to env var
   let config = require('./config.json');
   let envConfig = config[env];
 
   
-  
+  //Object.keys() returns array of property names of the passed object, then for each loops through that array and assings local environment variables...
+  //..like proces.env.PORT = envConfig.PORT
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key]
   })
 }
 
-//config[env] je bracket notation a podle toho jaka je hodnota env tak je to stejne jako config.development nebo config.test
-
-//envConfig je tedy bud objekt test nebo objekt development z config.json
-
-//Object.keys() vezme nejaky objekt a vsechny key hodi do array, ktere projdeme pomoci forEach a pomoci bracket notation nastavime potrebne parametry...
-// postupne to tedy udela process.env.PORT = test.PORT a proces.env.MONGODB_URI = '...TodoAppTest' a nebo ... = development.PORT a development.MONGODB_URI...
 
 
 
