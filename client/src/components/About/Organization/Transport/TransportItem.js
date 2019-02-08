5a36f960269342b254deeb68d6253ee4f802a59c
Vit Krajicek <vit@krajicek.org>
2019-02-08 14:50:36 +0100

    transport icon

--- client/src/components/About/Organization/Transport/TransportItem.js
+++ client/src/components/About/Organization/Transport/TransportItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TransportItem = (props) => (
     <div className='transport__item'>
-        <img src='/images/about/icons/request_icon2.svg' />
+        <img src={props.transport.img} />
         <h3>{props.transport.head}</h3>
         <p>{props.transport.text}</p>
     </div>
